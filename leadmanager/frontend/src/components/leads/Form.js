25b107998b5048a8703bba29ce8b45cab83c70e9
aabuiteiwi <aabuiteiwi@vubari.com>
2019-09-19 18:15:25 +0200

    http & redux done

--- leadmanager/frontend/src/components/leads/Form.js
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -1,11 +1,79 @@
-import React, { Component } from 'react'
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addLead } from "../../actions/leads";
 
-export default class Form extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Add Lead Form</h1>
-            </div>
-        )
-    }
+export class Form extends Component {
+  state = {
+    name: "",
+    email: "",
+    message: ""
+  };
+
+  static propTypes = {
+    addLead: PropTypes.func.isRequired
+  };
+
+  onChange = e =>
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+
+  onSubmit = e => {
+    e.preventDefault();
+    const { name, email, message } = this.state;
+    const lead = { name, email, message };
+    this.props.addLead(lead);
+  };
+
+  render() {
+    const { name, email, message } = this.state;
+    return (
+      <div className="card card-body mt-4 mb-4">
+        <h2>Add Lead</h2>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Name</label>
+            <input
+              type="text"
+              name="name"
+              className="form-control"
+              onChange={this.onChange}
+              value={name}
+            ></input>
+          </div>
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              onChange={this.onChange}
+              value={email}
+            ></input>
+          </div>
+          <div className="form-group">
+            <label>Message</label>
+            <textarea
+              className="form-control"
+              type="text"
+              name="message"
+              onChange={this.onChange}
+              value={message}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
+
+export default connect(
+  null,
+  { addLead }
+)(Form);
